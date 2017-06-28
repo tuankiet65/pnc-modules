987b2275b4ca2934ecabb1fd2c789520f5465527
Ho Tuan Kiet <tuankiet65@gmail.com>
2017-06-28 17:14:38 +0700

    lcd_16x2_block: Move to using DataPacketStream

--- src/lcd_16x2_block.cpp
+++ src/lcd_16x2_block.cpp
@@ -12,17 +12,21 @@ LCD_HD44780_16x2 lcd(PIN_PA1, PIN_PA0, PIN_PD2,
 	                 PIN_PB3, PIN_PB2, PIN_PB1, PIN_PB0,
 	                 PIN_PD6);
 
-data_packet rx_data;
-uint8_t rx_state = STATE_TYPE, rx_ptr;
+DataPacketStream dpstream(&Serial);
 
-void print(uint8_t n){
-	lcd.write(48 + (n / 10));
-	lcd.write(48 + (n % 10));
-	lcd.write(' ');
-}
+void command(data_packet rx){
+	data_packet response;
 
-void command(){
-	switch (rx_data.type){
+	response.type = RESP_SUCCESS;
+	response.data_len = 0;
+
+	switch (rx.type){
+		case CMD_INFO:
+			response.data_len = 3;
+			response.data[0] = MODULE_TYPE_LCD;
+			response.data[1] = 16;
+			response.data[2] = 2;
+			break;
 		case CMD_LCD_HOME:
 			lcd.home();
 			break;
@@ -30,82 +34,73 @@ void command(){
 			lcd.clear();
 			break;
 		case CMD_LCD_WRITE:
-			lcd.write((char*)rx_data.data, rx_data.data_len);
+			lcd.write((char*)rx.data, rx.data_len);
 			break;
 		case CMD_LCD_TOGGLE_DISPLAY:
-			if (rx_data.data_len != 1){
+			if (rx.data_len != 1){
+				response.type = RESP_FAILURE;
+				response.data_len = 1;
+				response.data[0] = ERROR_WRONG_NUMBER_OF_ARGUMENT;
 				break;
 			}
 
-			lcd.set_display(rx_data.data[0]);
+			lcd.set_display(rx.data[0]);
 			break;
 		case CMD_LCD_TOGGLE_CURSOR:
-			if (rx_data.data_len != 1){
+			if (rx.data_len != 1){
+				response.type = RESP_FAILURE;
+				response.data_len = 1;
+				response.data[0] = ERROR_WRONG_NUMBER_OF_ARGUMENT;
 				break;
 			}
 
-			lcd.set_cursor(rx_data.data[0]);
+			lcd.set_cursor(rx.data[0]);
 			break;
 		case CMD_LCD_TOGGLE_BLINK:
-			if (rx_data.data_len != 1){
+			if (rx.data_len != 1){
+				response.type = RESP_FAILURE;
+				response.data_len = 1;
+				response.data[0] = ERROR_WRONG_NUMBER_OF_ARGUMENT;
 				break;
 			}
 
-			lcd.set_blink(rx_data.data[0]);
+			lcd.set_blink(rx.data[0]);
 			break;
 		case CMD_LCD_SET_POSITION:
-			if (rx_data.data_len != 2){
+			if (rx.data_len != 2){
+				response.type = RESP_FAILURE;
+				response.data_len = 1;
+				response.data[0] = ERROR_WRONG_NUMBER_OF_ARGUMENT;
 				break;
 			}
 
-			lcd.set_position(rx_data.data[0], rx_data.data[1]);
+			lcd.set_position(rx.data[0], rx.data[1]);
+			break;
+		case CMD_LCD_TOGGLE_BACKLIGHT:
+			if (rx.data_len != 1){
+				response.type = RESP_FAILURE;
+				response.data_len = 1;
+				response.data[0] = ERROR_WRONG_NUMBER_OF_ARGUMENT;
+				break;
+			}
+
+			lcd.set_backlight(rx.data[0]);
 			break;
 		default:
+			response.type = RESP_FAILURE;
 			break;
 	}
+	dpstream.send(response);
 }
 
 int main(){
-	set_pin_mode(PIN_PD6, OUTPUT);
-	digital_write(PIN_PD6, HIGH);
-
 	lcd.begin();
 	Serial.begin(1200);
+
 	while (1){
-		if (Serial.available()){
-			switch (rx_state){
-				case STATE_TYPE:
-					rx_data.type = Serial.read();
-					rx_state = STATE_LEN;
-					break;
-				case STATE_LEN:
-					// read in len then move to STATE_DATA, ready to read data
-					rx_data.data_len = Serial.read();
-					if (rx_data.data_len > sizeof(rx_data.data)){
-						// size is too large, discarding
-						rx_state = STATE_TYPE;	
-					} else {
-						rx_ptr = 0;
-						rx_state = STATE_DATA;
-
-						if (rx_data.data_len == 0){
-							command();
-							rx_state = STATE_TYPE;
-						}
-					}
-					break;
-				case STATE_DATA:
-					// Read byte and increase pointer
-					rx_data.data[rx_ptr] = Serial.read();
-					rx_ptr++;
-
-					if (rx_ptr >= rx_data.data_len){
-						// All bytes have been received, calling handler
-						command();
-						rx_state = STATE_TYPE;
-					}
-					break;
-			}
+		dpstream.rx_loop();
+		if (dpstream.available()){
+			command(dpstream.read());
 		}
 	}
 }
