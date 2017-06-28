b1016fc197491392135fad64a5db11a5b4448b27
Ho Tuan Kiet <tuankiet65@gmail.com>
2017-06-28 17:12:05 +0700

    New DataPacketStream class
    DataPackerStream is a Stream-like (although not really inherited from Stream) for packet data

--- src/protocol/protocol.cpp
+++ src/protocol/protocol.cpp
@@ -1,1 +1,71 @@
 #include "protocol/protocol.h"
+
+void DataPacketStream::rx_loop(){
+	if (stream == nullptr){
+		return;
+	}
+	
+	if (stream->available()){
+		empty_cycle_count = 0;
+		unread = false;
+		switch (rx_state){
+			case STATE_TYPE:
+				rx_data.type = stream->read();
+				rx_state = STATE_LEN;
+				break;
+			case STATE_LEN:
+				// read in len then move to STATE_DATA, ready to read data
+				rx_data.data_len = stream->read();
+				if (rx_data.data_len > sizeof(rx_data.data)){
+					// size is too large, discarding
+					rx_state = STATE_TYPE;
+					fail(ERROR_SIZE_TOO_LARGE);
+				} else {
+					rx_ptr = 0;
+					rx_state = STATE_DATA;
+
+					if (rx_data.data_len == 0){
+						unread = true;
+						rx_state = STATE_TYPE;
+					}
+				}
+				break;
+			case STATE_DATA:
+				// Read byte and increase pointer
+				rx_data.data[rx_ptr] = stream->read();
+				rx_ptr++;
+
+				if (rx_ptr >= rx_data.data_len){
+					// All bytes have been received, calling handler
+					unread = true;
+					rx_state = STATE_TYPE;
+				}
+
+				break;
+		}
+	} else {
+		empty_cycle_count++;
+	}
+
+	if (rx_state != STATE_TYPE && empty_cycle_count >= MAX_EMPTY_CYCLE_COUNT){
+		// timeout, quitting
+		fail(ERROR_TIMEOUT);
+		rx_state = STATE_TYPE;
+		empty_cycle_count = 0;
+	}
+}
+
+void DataPacketStream::send(data_packet &tx){
+	uint8_t i;
+	stream->send(tx.type);
+	stream->send(tx.data_len);
+	for (i = 0; i < tx.data_len; i++){
+		stream->send(tx.data[i]);
+	}
+}
+
+void DataPacketStream::fail(uint8_t error_type){
+	stream->send(RESP_FAILURE);
+	stream->send(0x01);
+	stream->send(error_type);
+}
--- src/protocol/protocol.h
+++ src/protocol/protocol.h
@@ -2,13 +2,50 @@
 	
 	#define PROTOCOL_H
 	
+	#include "uart/stream.h"
+	
 	#define PROTOCOL_BAUD 9600
 	
+	#define STATE_IDLE 0
+	#define STATE_TYPE 1
+	#define STATE_LEN 2
+	#define STATE_DATA 3
+	
+	#define MAX_EMPTY_CYCLE_COUNT 255
+
 	typedef struct {
 		uint8_t type;
 		uint8_t data_len;
 		uint8_t data[8];
 	} data_packet;
+
+	class DataPacketStream {
+	private:
+		Stream *stream = nullptr;
+		data_packet rx_data;
+		uint8_t rx_state = STATE_TYPE, rx_ptr;
+		bool unread = false;
+		uint8_t empty_cycle_count = 0;
+
+		void fail(uint8_t error_type);
+	public:
+		DataPacketStream(Stream *stream) {
+			this->stream = stream;
+		}
+
+		void rx_loop();
+
+		bool available(){
+			return unread;
+		}
+
+		data_packet read(){
+			unread = false;
+			return rx_data;
+		}
+		
+		void send(data_packet &tx);
+	};
 
 	#define CMD_INFO 0
 	#define CMD_LCD_WRITE 1
@@ -18,9 +55,15 @@
 	#define CMD_LCD_TOGGLE_CURSOR 5
 	#define CMD_LCD_TOGGLE_BLINK 6
 	#define CMD_LCD_SET_POSITION 7
+	#define CMD_LCD_TOGGLE_BACKLIGHT 8
 
 	#define RESP_SUCCESS 0
 	#define RESP_FAILURE 1
 	#define RESP_WORKING 2
 
+	#define ERROR_TIMEOUT 0
+	#define ERROR_SIZE_TOO_LARGE 1
+	#define ERROR_WRONG_NUMBER_OF_ARGUMENT 2
+
+	#define MODULE_TYPE_LCD 1
 #endif
